60eac32158f882d24d1c0b0488b2a385437500e9
Alexandros Spartalis <alexandros.spartalis@eficode.com>
2020-07-01 17:08:42 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -70,7 +70,8 @@ docker-compose rm -fsv
   # CTT: Create Project
  #DOWN is the one that works
 #export CTT_PROJECT_UUID=$(./curl_uuid.sh \"${CTT_ENDPOINT}/project\" \"{\\\"name\\\":\\\"use-case-radon-demo\\\",\\\"repository_url\\\":\\\"${REPO_DEMO_URL}\\\"}\")
-export CTT_PROJECT_UUID=$(./curl_uuid.sh "${CTT_ENDPOINT}/project" "{\\\"name\\\":\\\"SockShop\\\",\\\"repository_url\\\":\\\"${SOCKSHOP_DEMO_URL}\\\"}")
+#export CTT_PROJECT_UUID=$(./curl_uuid.sh "${CTT_ENDPOINT}/project" "{\\\"name\\\":\\\"SockShop\\\",\\\"repository_url\\\":\\\"${SOCKSHOP_DEMO_URL}\\\"}")
+export CTT_PROJECT_UUID=$(curl -X POST "${CTT_ENDPOINT}/project" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"name\":\"use-case-radon-demo-AlexSpart\",\"repository_url\":\"https://github.com/AlexSpart/use-case-radon-demo.git\"}")
 echo "CTT_PROJECT_UUID: ${WORKSPACE}"
 echo "CTT_PROJECT_UUID: ${CTT_PROJECT_UUID}"
   # Copy CSARs into project
