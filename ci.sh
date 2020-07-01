a83e6b85e16d1c22b7f17b123d239d2523fc09e9
Alexandros Spartalis <alexandros.spartalis@eficode.com>
2020-07-01 17:38:02 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -75,7 +75,7 @@ export CTT_PROJECT_UUID=$(./curl_uuid.sh "${CTT_ENDPOINT}/project"  "{\"name\":\
 echo "CTT_PROJECT_UUID: ${WORKSPACE}"
 echo "CTT_PROJECT_UUID: ${CTT_PROJECT_UUID}"
   # Copy CSARs into project
-mkdir ${CTT_VOLUME}/project/${CTT_PROJECT_UUID}/radon-ctt
+mkdir -p ${CTT_VOLUME}/project/${CTT_PROJECT_UUID}/radon-ctt
 cp "${SUT_CSAR}" "${TI_CSAR}" "${CTT_VOLUME}/project/${CTT_PROJECT_UUID}/radon-ctt/."
 ls ${CTT_VOLUME}/project/${CTT_PROJECT_UUID}/radon-ctt
   # CTT: Create Test-Artifact
